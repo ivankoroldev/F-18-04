4302c8de2a14479ff578c419d3ad22c4e0e02c95
UABilostVi <bilostotskyi1991@gmail.com>
2019-01-03 22:06:48 +0200

    vbilostotskyi:slider is done

---
+++ homework/vbilostotskyi/hw-3/js/javascripst.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+       slidesToShow: 1,
+       slidesToScroll: 1,
+       dots: true,
+    //    autoplay: true,
+       autoplaySpeed: 4000,
+       arrows: false
+    });
+  });
